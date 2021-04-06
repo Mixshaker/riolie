ba7f1705711283349ca93843ef91f3eed90f1993
Rostislav <rostislav.lobanok@gmail.com>
2021-04-07 01:31:04 +0300

    Podcasts integrated

--- js/script.js
+++ js/script.js
@@ -17,9 +17,62 @@ document.addEventListener("DOMContentLoaded", function() {
             heroText.style.textShadow = `${deltaX}em ${deltaY}em 0.1em #01ecff`
             heroText.style.textShadow = `${deltaX}em ${deltaY}em 0.1em #01ecff`
             heroText.style.transform = `rotateZ(${deltaX * 25}deg) skew(${deltaX * 16}deg, ${deltaX * 16}deg)`
-
-            console.log('Coords:', x, y);
-            console.log('Coords:', deltaX, deltaY);
         })
     }
-})
+});
+
+// Mixcloud
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    let url = "//api.mixcloud.com/riolie/cloudcasts/?limit=15";
+
+    async function getInfo(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // console.log(data);
+
+        let podcasts = document.getElementById('podcasts');
+
+
+        data.data.forEach(function(el) {
+
+            let cover = document.createElement('img');
+            cover.src = el.pictures['640wx640h'];
+            cover.dataset.mixcloudPlayButton = el.url;
+            podcasts.append(cover);
+
+        });
+
+        // let paging = document.createElement('a');
+        let paging;
+        if (data.paging.next) {
+            paging = document.getElementById('paging') ? document.getElementById('paging') : document.createElement('a');
+            paging.href = data.paging.next;
+            paging.id = "paging";
+            paging.className = "paging";
+            paging.innerText = "Load more";
+            podcasts.after(paging);
+        } else {
+            document.getElementById('paging').remove();
+        }
+
+    };
+
+    getInfo(url).then(function() {
+        // Load more
+        let pagingBtn = document.getElementById('paging');
+
+        pagingBtn.addEventListener("click", function(e) {
+            e.preventDefault();
+            url = this.href;
+
+            getInfo(url);
+        });
+    });
+
+
+
+
+});
